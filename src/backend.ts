00fb0d96e1da0f7b18841fad03dc46baaf281872
Ethan-Schmitt <145645263+Ethan-Schmitt@users.noreply.github.com>
2025-02-18 14:51:06 +0100

    mettre à jour la connexion à Pocketbase pour utiliser l'URL configurée dans les variables d'environnement

--- src/backend.ts
+++ src/backend.ts
@@ -1,3 +1,4 @@
 import Pocketbase from 'pocketbase';
-export const  pb = new Pocketbase("http://0.0.0.0:8090")
+import type { TypedPocketBase } from './pocketbase-typegen';
+export const  pb = new Pocketbase(import.meta.env.VITE_URL_POCKETBASE) as TypedPocketBase;
 
